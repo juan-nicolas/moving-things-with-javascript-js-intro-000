53787aac4c446717cef82bf6b8497c5cea1fda61
Juan Nicolas <jnma91@gmail.com>
2018-10-20 00:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// console.log('hello?')
-// 
+console.log('hello?')
+//
 // document.addEventListener('keydown', function(e){
 // 	if(e.which === 37){
 // 		var leftNum = dodger.style.left.replace('px','')
 // 		var left = parseInt(leftNum, 10)
 // 		console.log(leftNum)
 // 		console.log(left)
-// 
+//
 // 		dodger.style.left = '${left-1}px'
 // 		console.log(dodger.style.left)
 // 	}
