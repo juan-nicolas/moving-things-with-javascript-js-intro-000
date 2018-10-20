7650c7d0f75a6683ba5033921215f3e1cac40776
Juan Nicolas <jnma91@gmail.com>
2018-10-20 01:00:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 console.log('hello?')
-//
-// document.addEventListener('keydown', function(e){
-// 	if(e.which === 37){
-// 		var leftNum = dodger.style.left.replace('px','')
-// 		var left = parseInt(leftNum, 10)
-// 		console.log(leftNum)
-// 		console.log(left)
-//
-// 		dodger.style.left = '${left-1}px'
-// 		console.log(dodger.style.left)
-// 	}
-// })
+
+var dodger = document.getElementById('dodger')
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'ArrowLeft'){
+    var leftNum = dodger.style.left.replace('px','')
+    var left = parseInt(leftNum, 10)
+		console.log(leftNum)
+		console.log(left)
+
+		dodger.style.left = '${left-1}px'
+		console.log(dodger.style.left)
+	}
+})
