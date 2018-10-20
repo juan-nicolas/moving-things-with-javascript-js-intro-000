728fb54f777b667cd590c0540c4935bf63fcbd1c
Juan Nicolas <jnma91@gmail.com>
2018-10-20 01:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var speed = 1
 function moveDodgerLeft(){
   var leftNum = dodger.style.left.replace('px','')
   var left = parseInt(leftNum, 10)
-
-  dodger.style.left = `${left-speed}px`
+  if (left > 0){
+    if (left > speed){
+      dodger.style.left = `${left-speed}px`
+    } else {
+      dodger.style.left = `0px`
+    }
+  }      
 }
 
 document.addEventListener('keydown', function(e){
