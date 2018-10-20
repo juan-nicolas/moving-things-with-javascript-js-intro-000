bcb326b87b9efa5123084a1d98ef0870309aace4
Juan Nicolas <jnma91@gmail.com>
2018-10-20 00:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const app = "I don't do much."
+console.log('hello?')
+
+document.addEventListener('keydown', function(e){
+	if(e.which === 37){
+		var leftNum = dodger.style.left.replace('px','')
+		var left = parseInt(leftNum, 10)
+		console.log(leftNum)
+		console.log(left)
+		
+		dodger.style.left = '${left-1}px'
+		console.log(dodger.style.left)
+	}
+})
